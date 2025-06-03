ddd71f1726d79e340f7834f1fab56338aab97222
U2D-8Bits <u2d.8bits@gmail.com>
2025-06-03 07:31:15 -0500

    feat: añadir el componente CustomButton con plantilla, estilos y pruebas

---
+++ src/app/shared/components/custom-button/custom-button.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-button',
+  templateUrl: './custom-button.component.html',
+  styleUrls: ['./custom-button.component.css'],
+  standalone: true,
+  imports: []
+})
+export class CustomButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
