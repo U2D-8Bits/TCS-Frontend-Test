cab32c3e261c05925e91a9657c170e3b8b1290c3
U2D-8Bits <u2d.8bits@gmail.com>
2025-06-03 08:32:04 -0500

    feat: agregar estructura y estilos al componente Product, incluyendo tabla y menú de acciones

--- src/app/features/product/product.component.ts
+++ src/app/features/product/product.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomButtonComponent } from '../../shared/components/custom-button/custom-button.component';
+import { CustomInputComponent } from '../../shared/components/custom-input/custom-input.component';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css'],
   standalone: true,
-  imports: []
+  imports: [
+    // CustomButtonComponent,
+    CustomInputComponent
+  ]
 })
 export class ProductComponent implements OnInit {
 
