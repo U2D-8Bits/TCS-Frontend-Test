c5c2592bc1e733d5440e4357f6a37f4820810967
U2D-8Bits <u2d.8bits@gmail.com>
2025-06-03 08:26:00 -0500

    feat: implementar el componente CustomInput con validación y estilo

--- src/app/shared/components/custom-input/custom-input.component.ts
+++ src/app/shared/components/custom-input/custom-input.component.ts
@@ -1,17 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { NgIf, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-custom-input',
   templateUrl: './custom-input.component.html',
   styleUrls: ['./custom-input.component.css'],
   standalone: true,
-  imports: []
+  imports: [NgIf, NgClass],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CustomInputComponent),
+      multi: true
+    }
+  ]
 })
-export class CustomInputComponent implements OnInit {
+export class CustomInputComponent implements OnInit, ControlValueAccessor {
+  @Input() placeholder: string = '';
+  @Input() label: string = '';
+  @Input() errorMessage: string = '';
+  @Input() type: string = 'text';
+
+  value: string = '';
+  touched = false;
+  disabled = false;
+  showError = false;
+
+  onChange = (value: any) => {};
+  onTouched = () => {};
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
   }
 
+  handleInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.value = input.value;
+    this.onChange(this.value);
+    this.showError = false;
+  }
+
+  handleBlur() {
+    this.touched = true;
+    this.onTouched();
+    this.showError = !this.value && !!this.errorMessage;
+  }
 }
