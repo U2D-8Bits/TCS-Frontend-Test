bd9f8edd45ea42ac92712fa8ce3cdcc2e6786f3a
U2D-8Bits <u2d.8bits@gmail.com>
2025-06-03 07:34:31 -0500

    feat: definir rutas de aplicación para el componente de producto y la redirección

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,26 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('./features/product/product.component').then((m) => m.ProductComponent),
+    children: [
+      {
+        path: ':id',
+        loadComponent: () =>
+          import('./features/product/product.component').then((m) => m.ProductComponent),
+      },
+    ],
+  },
+  {
+    path: '',
+    redirectTo: 'products',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'products',
+    pathMatch: 'full',
+  }
 ];
