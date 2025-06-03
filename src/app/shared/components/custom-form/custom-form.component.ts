075a384f52dd4e93ff5d94d0be549075272c197b
U2D-8Bits <u2d.8bits@gmail.com>
2025-06-03 08:03:02 -0500

    feat: añadir componente CustomForm con plantilla, estilos y configuración de prueba

---
+++ src/app/shared/components/custom-form/custom-form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-form',
+  templateUrl: './custom-form.component.html',
+  styleUrls: ['./custom-form.component.css'],
+  standalone: true,
+  imports: []
+})
+export class CustomFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
